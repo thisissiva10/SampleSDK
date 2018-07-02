3b24aed988889246958c5500c6bb709431ac3ff2
s-ramprakash <sramprakash18@gmail.com>
2018-07-02 19:00:09 +0530

    final commit

---
+++ SampleSDK/CRM/Library/Api/APIStats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace SampleSDK.CRM.Library.Api
+{
+    public class APIStats
+    {
+        private static Dictionary<string, string> apiCountStats = new Dictionary<string, string>();
+
+        public APIStats() { }
+
+        public static void UpdateStats(HttpWebResponse response)
+        {
+            UpdateCountStats(response);
+        }
+
+        private static void UpdateCountStats(HttpWebResponse response)
+        {
+            apiCountStats.Add("X-RATELIMIT-LIMIT", response.GetResponseHeader("X-RATELIMIT-LIMIT"));
+            apiCountStats.Add("X-RATELIMIT-REMAINING", response.GetResponseHeader("X-RATELIMIT-REMAINING"));
+            apiCountStats.Add("X-RATELIMIT-RESET", response.GetResponseHeader("X-RATELIMIT-RESET"));
+        }
+
+        public static string GetRemainingCountForTheDay()
+        {
+            return apiCountStats["X-RATELIMIT-LIMIT"];
+        }
+
+        public static string GetRemainingCountForCurrentWindow()
+        {
+            return apiCountStats["X-RATELIMIT-REMAINING"];
+        }
+        public static string GetRemainingTimeForWindowReset()
+        {
+            return apiCountStats["X-RATELIMIT-RESET"];
+        }
+    }
+}
